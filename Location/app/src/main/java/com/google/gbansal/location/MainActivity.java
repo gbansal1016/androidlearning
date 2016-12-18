2b9a35fa762a872de61f7b01e2b6ca6cdbf0eb85
Gaurav Bansal <gauravkbansal@gmail.com>
2016-12-18 00:41:59 -0500

    Updated working version

--- Location/app/src/main/java/com/google/gbansal/location/MainActivity.java
+++ Location/app/src/main/java/com/google/gbansal/location/MainActivity.java
@@ -1,9 +1,13 @@
 package com.google.gbansal.location;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,15 +16,23 @@ import android.widget.TextView;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity
-        implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener{
+        implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
 
-    private static final String TAG=MainActivity.class.getSimpleName();
+    private static final String TAG = MainActivity.class.getSimpleName();
     private GoogleApiClient gClient;
 
+    private Location mLastLocation;
+
     private TextView locationView;
+
+    private LocationRequest mLocationRequest;
+
+    private final int PERMISSION_REQUEST_LOCATION_ACCESS = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,8 +63,62 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
+    public void onConnected(Bundle bundle) {
         Log.i(TAG, "Google API Client connected");
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION_ACCESS);
+        } else {
+            Log.i(TAG, "Access to location already available");
+            updateLastLocation();
+            if (mLastLocation != null) {
+                locationView.setText("Latitude:" + mLastLocation.getLatitude() + "Longitude:" + mLastLocation.getLongitude());
+            }
+            createLocationRequest();
+        }
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(2000);
+        LocationServices.FusedLocationApi.requestLocationUpdates(gClient, mLocationRequest, this);
+    }
+
+    private void updateLastLocation() {
+
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(gClient);
+        int retry = 0;
+        while ((mLastLocation == null) && retry <= 10) {
+            retry++;
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+            }
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(gClient);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_LOCATION_ACCESS:
+                //Permission granted
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "Permission granted for locations");
+
+                    updateLastLocation();
+                    if (mLastLocation != null) {
+                        locationView.setText("Latitude:" + mLastLocation.getLatitude() + "Longitude:" + mLastLocation.getLongitude());
+                    }
+                    createLocationRequest();
+                }
+                break;
+            default:
+                Log.e(TAG, "Permission to access location is denied");
+                break;
+        }
     }
 
     @Override
@@ -63,6 +129,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onLocationChanged(Location location) {
         Log.i(TAG, "Location Changed");
-        locationView.setText("Latitude:" + location.getLatitude()+ "Longitude:" + location.getLongitude());
+        locationView.setText("Latitude:" + location.getLatitude() + "Longitude:" + location.getLongitude());
     }
 }
